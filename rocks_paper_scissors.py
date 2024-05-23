e119f1bd1dc47eace225078a7516c11a76cc9463
Fractal Plane <fractalplane@scotts-mbp-2.lan>
2024-05-22 22:10:22 -0700

    adding my first python projects

---
+++ rocks_paper_scissors.py
@@ -0,0 +1,36 @@
+#!/usr/local/bin/python3
+
+import random
+
+user_action = None
+possible_actions = ['rock', 'paper', 'scissors']
+
+while True:
+    
+    while user_action not in possible_actions:
+        user_action = input('Enter a choice (rock, paper, scissors): ')
+    
+    computer_action = random.choice(possible_actions)
+    
+    print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
+       
+    if user_action == computer_action:
+        print(f"Both players selected {user_action}. It's a tie!")
+    elif user_action == 'rock':
+        if computer_action == "scissors":
+            print('Rock smashed scissors! You win!')
+        else:
+            print('Paper covers rock! You lose.')
+    elif user_action == 'paper':
+        if computer_action == 'rock':
+            print('Paper covers rock! You win!')
+        else:
+            print('Scissors cut paper! You lose.')
+    elif user_action == 'scissors':
+        if computer_action == "paper":
+            print('Scissors cut paper! You win!')
+        else:
+            print('Rock smashed scissors! You lose.')
+    play_again = input('Play again? (y/n): ')
+    if play_again.lower() != 'y':
+        break
