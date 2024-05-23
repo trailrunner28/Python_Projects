e054ad73b13c728049b0e658acbebafedda60262
Fractal Plane <fractalplane@scotts-mbp-2.lan>
2024-05-22 23:02:54 -0700

    adding blackjack to github

---
+++ blackjack.py
@@ -0,0 +1,43 @@
+#/usr/local/bin/python3
+
+
+import random
+
+# BlackJack Plan
+# 14 cards x4 =deck
+# j=11
+# q=12
+# k=13
+# A=14
+#
+# dealer=
+# player=
+# import random
+#
+# deal two cards
+# dealer shows second card
+# count card value
+#
+# options hit or stay
+#
+# dealer shows second card
+# if dealer is < 17: then
+# dealer always hits
+#
+# if hand is < 11 : then
+#     ace equals 11
+# else:
+#      ace equals 1
+#
+# compare totals
+#
+# if dealer amount = player amount: then
+# push
+# if dealer closer to or equal to 21:
+# you lose
+# if player is closer to or equal to 21:
+# you win
+# if player > 21 : then
+# bust
+# if player has [11,12,13] plus 14: then
+# BLACKJACK!
