5556491dbb70a5f9d36c49990b705141f4bf3222
Fractal Plane <fractalplane@Scotts-MBP-2.attlocal.net>
2024-06-16 07:56:55 -0700

    added conditions for blackjack and rock_paper_scissors

--- blackjack.py
+++ blackjack.py
@@ -18,10 +18,10 @@ def calculate_total(hand):
             total -= 10
         return total
         
-def players_turn(deck, player_hand):
-    while true:
+def player_turn(deck, player_hand):
+    while True:
         player_choice = input( "Hit or Stand? ").lower()
-        if play_choice == 'hit':
+        if player_choice == 'hit':
             player_hand.append(deal_card(deck))
             total = calculate_total(player_hand)
             print("Your hand:", player_hand, "Total:", total)
@@ -29,46 +29,43 @@ def players_turn(deck, player_hand):
                 print("Bust! You lost.")
                 return False
         elif player_choice == 'stand':
-            return true
+            return True
 
 def dealers_turn(deck, dealer_hand):
-    while calculate_total(dealer_hand(deck) < 17:
+    while calculate_total(dealer_hand) < 17:
         dealer_hand.append(deal_card(deck))
     return dealer_hand
     
 while True:
-    player_hand = [deal_card(deck), deal_card(deck)]
-    dealer_hand = [deal_card(deck), deal_card(deck)]
+    player_hand = [ deal_card(deck), deal_card(deck) ]
+    dealer_hand = [ deal_card(deck), deal_card(deck) ]
     
     print("Your hand:", player_hand, "Total", calculate_total(player_hand))
-print("Dealer's hand:", dealer_hand[0])
-
-#Player's Turn
-if player_turn(deck, player_hand):
-    dealer_hand = dealer_turn(deck, dealer_hand)
-    print("Dealer's hand:", dealer_hand, "Total:", calculate_total(dealer_hand))
-    
+    print("Dealer's hand:", dealer_hand[0])
+    #Player's Turn
+    if player_turn(deck, player_hand):
+        dealer_hand = dealers_turn(deck, dealer_hand)
+        print("Dealer's hand:", dealer_hand, "Total:", calculate_total(dealer_hand))
     #determine winner
     player_total = calculate_total(player_hand)
     dealer_total = calculate_total(dealer_hand)
-    if dealer_total > 21 or player_total > dealer_total:
+    if dealer_total > 21:
         print("You Win!!")
-    elif dealer_total > player_total:
+    if player_total <= 21 and player_total > dealer_total:
+        print("You Win!!")
+    elif dealer_total <= 21 and dealer_total > player_total:
         print("Dealer Wins!")
-    else:
-        print("It's a tie")
-
-# Check if player wants to play again
-play_again = input("Play again? (yes/no): ").lower()
-if play_again != 'yes':
-    break
-    
-#Re-shuffle the deck if it is running low
-if len(deck) < 10:
-    deck = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'] * 4
-    random.shuffle(deck)
-    
-print("Thanks for playing!")
+    if dealer_total == player_total:
+        print("It's a Tie")
+    #check if enough cards are available to play a hand and reshuffle if not
+    if len(deck) < 10:
+       deck = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'] * 4
+       random.shuffle(deck)
+    # Check if player wants to play again
+    play_again = input("Play again? (yes/no): ").lower()
+    if play_again != 'yes':
+        break
+print("Thanks For Playing!")
 
 
 #setup a dictionary of the cards with the key and value
--- rocks_paper_scissors.py
+++ rocks_paper_scissors.py
@@ -4,13 +4,15 @@ import random
 
 user_action = None
 possible_actions = ['rock', 'paper', 'scissors']
+user_winner = 0
+computer_winner = 0
 
 while True:
     
     while user_action not in possible_actions:
         user_action = input('Enter a choice (rock, paper, scissors): ')
     
-    computer_action = random.choice(possible_actions)
+        computer_action = random.choice(possible_actions)
     
     print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
        
@@ -18,19 +20,29 @@ while True:
         print(f"Both players selected {user_action}. It's a tie!")
     elif user_action == 'rock':
         if computer_action == "scissors":
+            user_winner += 1
             print('Rock smashed scissors! You win!')
         else:
+            computer_winner += 1
             print('Paper covers rock! You lose.')
     elif user_action == 'paper':
         if computer_action == 'rock':
+            user_winner += 1
             print('Paper covers rock! You win!')
         else:
+            computer_winner += 1
             print('Scissors cut paper! You lose.')
     elif user_action == 'scissors':
         if computer_action == "paper":
+            user_winner += 1
             print('Scissors cut paper! You win!')
         else:
+            computer_winner += 1
             print('Rock smashed scissors! You lose.')
     play_again = input('Play again? (y/n): ')
-    if play_again.lower() != 'y':
+    if play_again.lower() == 'y':
+        user_action = input('Enter a choice (rock, paper, scissors): ')
+    else:
+        print("Thanks for Playing!")
         break
+        
